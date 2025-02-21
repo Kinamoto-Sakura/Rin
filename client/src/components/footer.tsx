8bba36caa715921291a0fcfef8fa3d87184d3e31
Sakura <clgzh3060@gmail.com>
2025-02-21 20:24:46 +0800

    Update footer.tsx

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -56,17 +56,7 @@ function Footer() {
                 </div>
                                 <p className='text-sm text-neutral-500 font-normal link-line text-center'>
                 //添加备案
-                    <br/> <a className='hover:underline' href="https://beian.miit.gov.cn" target="_blank">冀ICP备2024090725号</a>
-<br/> 
-<a className='hover:underline' href="https://beian.mps.gov.cn/#/query/webSearch?code=13073302000040" target="_blank">
-  <img 
-    src="https://beian.mps.gov.cn/web/assets/logo01.6189a29f.png" 
-    alt="备案图标" 
-    style={{ display: 'inline-block', width: '16px', height: '16px', verticalAlign: 'middle', marginRight: '4px' }} 
-  />
-  冀公网安备13073302000040
-</a>
-                    <br/> 
+            <br/> <a className='hover:underline' href="https://beian.miit.gov.cn" target="_blank">冀ICP备2024090725号</a><br/><a className='hover:underline' href="https://beian.mps.gov.cn/#/query/webSearch?code=13073302000040" target="_blank"><img src="https://beian.mps.gov.cn/web/assets/logo01.6189a29f.png" alt="备案图标" style={{ display: 'inline-block', width: '16px', height: '16px', verticalAlign: 'middle', marginRight: '4px' }} />冀公网安备13073302000040</a><br/> 
                     <span onDoubleClick={() => {
                         if(doubleClickTimes >= 2){ // actually need 3 times doubleClick
                             setDoubleClickTimes(0)
