c3fe872fe739c65bc9f5d662da56e147b17ba5a0
Sakura <clgzh3060@gmail.com>
2025-02-21 20:29:59 +0800

    Update footer.tsx

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -48,7 +48,7 @@ function Footer() {
             </Helmet>
             <div className="flex flex-col mb-8 space-y-2 justify-center items-center t-primary ani-show">
                 {footerHtml && <div dangerouslySetInnerHTML={{ __html: footerHtml }} />}
-                <!--移动夜间模式位置-->
+                {/*移动夜间模式位置*/}
                     <div className="w-fit-content inline-flex rounded-full border border-zinc-200 p-[3px] dark:border-zinc-700">
                     <ThemeButton mode='light' current={modeState} label="Toggle light mode" icon="ri-sun-line" onClick={setMode} />
                     <ThemeButton mode='system' current={modeState} label="Toggle system mode" icon="ri-computer-line" onClick={setMode} />
