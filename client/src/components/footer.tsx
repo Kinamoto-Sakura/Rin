88e295a0c7b5e9e85ccff5b10949cd00ab67f63b
Sakura <clgzh3060@gmail.com>
2025-02-21 21:15:35 +0800

    Update footer.tsx

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -56,7 +56,7 @@ function Footer() {
                     <ThemeButton mode='dark' current={modeState} label="Toggle dark mode" icon="ri-moon-line" onClick={setMode} /></div>
                     <p className='text-sm text-neutral-500 font-normal link-line text-center'>
                 {/* 添加站点访问量统计代码 */}
-
+<br/><span>本站总访问量 <span id="busuanzi_value_site_pv" style="color: rgb(13, 109, 252); font-weight: bold;"></span>次 本站总访客数 <span id="busuanzi_value_site_uv" style="color: rgb(13, 109, 252); font-weight: bold;"></span> 人</span><br/>
                 {/*添加备案信息*/}
 <br/> <a className='hover:underline' href="https://beian.miit.gov.cn" target="_blank">冀ICP备2024090725号</a><br/><a className='hover:underline' href="https://beian.mps.gov.cn/#/query/webSearch?code=13073302000040" target="_blank"><img src="https://beian.mps.gov.cn/web/assets/logo01.6189a29f.png" alt="备案图标" style={{ display: 'inline-block', width: '16px', height: '16px', verticalAlign: 'middle', marginRight: '4px' }} />冀公网安备13073302000040</a><br/> 
                     <span onDoubleClick={() => {
